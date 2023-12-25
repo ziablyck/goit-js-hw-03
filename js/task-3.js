e79156e716f42fc8bbed5e3cc736d8bdf0f5b0ad
ziablyck <monechca@gmail.com>
2023-12-26 01:17:46 +0200

    adding hw-3js

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function filterArray(numbers, value) {
+    let array = [];
+    
+    for (let num of numbers) {
+        if (num > value) {
+            array.push(num);
+        }
+    }
+    return array;
+}
+    
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
